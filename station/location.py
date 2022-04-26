cb3d9f824222de039bb869c9c4a85b28f69cba03
guyong <guyongzx@163.com>
2022-04-26 18:26:51 +0800

    店铺信息输出优化

--- station/location.py
+++ station/location.py
@@ -5,6 +5,7 @@
 import json
 import requests
 from time import sleep
+import datetime
 
 authToken = ""
 
@@ -44,19 +45,56 @@ def getRecommendStoreListByLocation(longitude, latitude):
             if storeList[i].get("storeId") != '9991' and storeList[i].get("storeId") != '9996':
                 key = str(storeList[i].get("storeId")) + "==" + str(storeList[i]['storeRecmdDeliveryTemplateData']['storeDeliveryTemplateId']) + "--" + str(storeList[i].get("storeName"))
                 print(key)
-                # print(storeList[i])
-                # print(json.dumps(storeList[i]))
-                storeList_item[key] = {
-                    'storeType': storeList[i].get("storeType"),
-                    'storeId': storeList[i].get("storeId"),
-                    'areaBlockId': storeList[i].get('storeAreaBlockVerifyData').get("areaBlockId"),
-                    'storeDeliveryTemplateId': storeList[i].get('storeRecmdDeliveryTemplateData').get(
-                        "storeDeliveryTemplateId"),
-                    'deliveryModeId': storeList[i].get('storeDeliveryModeVerifyData').get("deliveryModeId"),
-                    'storeName': storeList[i].get("storeName")
-                }
 
-                fw.write(str(longitude) + ", " + str(latitude) + " " + key + ":" + json.dumps(storeList_item[key]) + "\n")
+                # 加入performanceTemplateName
+                performanceTemplateName = getDeliveryInfo(str(storeList[i]['storeRecmdDeliveryTemplateData']['storeDeliveryTemplateId']))
+                print(performanceTemplateName)
+                storeList_item[key] = "performanceTemplateName: " + str(performanceTemplateName) + " storeType: " + str(storeList[i].get("storeType")) + " storeId: " + str(storeList[i].get("storeId")) + " areaBlockId: " + str(storeList[i].get('storeAreaBlockVerifyData').get("areaBlockId")) + " deliveryModeId:" + str(storeList[i].get('storeDeliveryModeVerifyData').get("deliveryModeId"))
+                # storeList_item[key] = {
+                #     'performanceTemplateName': performanceTemplateName,
+                #     'storeType': storeList[i].get("storeType"),
+                #     'storeId': storeList[i].get("storeId"),
+                #     'areaBlockId': storeList[i].get('storeAreaBlockVerifyData').get("areaBlockId"),
+                #     'storeDeliveryTemplateId': storeList[i].get('storeRecmdDeliveryTemplateData').get(
+                #         "storeDeliveryTemplateId"),
+                #     'deliveryModeId': storeList[i].get('storeDeliveryModeVerifyData').get("deliveryModeId"),
+                #     'storeName': storeList[i].get("storeName")
+                # }
+                fw.write(str(longitude) + ", " + str(latitude) + " " + key + ":" + storeList_item[key] + "\n")
+
+def getDeliveryInfo(storeDeliveryTemplateId):
+    myUrl = 'https://api-sams.walmartmobile.cn/api/v1/sams/delivery/portal/getCapacityData'
+    headers = {
+        'Host': 'api-sams.walmartmobile.cn',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Content-Length': '45',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'User-Agent': 'SamClub/5.0.45 (iPhone; iOS 15.4; Scale/3.00)',
+        'device-name': 'iPhone14,3',
+        'device-os-version': '15.4',
+        'device-type': 'ios',
+        'auth-token': authToken,
+        'app-version': '5.0.45.1'
+    }
+
+    data = {
+        "perDateList": date_list,
+        "storeDeliveryTemplateId": storeDeliveryTemplateId
+    }
+    try:
+        requests.packages.urllib3.disable_warnings()
+        ret = requests.post(url=myUrl, headers=headers, data=json.dumps(data), verify=False)
+        myRet = ret.json()
+        if not myRet['success']:
+            return "fail"
+        else:
+            return myRet['data']['getPortalPerformanceTemplateResponse']['performanceTemplateName']
+
+    except Exception as e:
+        print('getCapacityData [Error]: ' + str(e))
 
 # getRecommendStoreListByLocation(121,31)
 storeList_item = {}
@@ -65,6 +103,11 @@ longEnd = 12194
 
 laStart = 3090
 laEnd = 3142
+
+# 查询配送信息的一周动态组装
+date_list = []
+for i in range(0, 7):
+    date_list.append((datetime.datetime.now() + datetime.timedelta(days=i)).strftime('%Y-%m-%d'))
 
 fw = open('log.txt', 'w')
 
@@ -80,7 +123,5 @@ fw.close()
 fw = open('list.txt', 'w')
 for k, v in storeList_item.items():
     fw.write(k + ":" + json.dumps(v) + "\n")
-
 fw.close()
-
 
